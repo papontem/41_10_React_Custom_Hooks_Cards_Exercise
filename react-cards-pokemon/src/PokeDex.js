57e3845a8198549e789654e12a20b2239a0cc8f0
papontemjetix <papontem@gmail.com>
2023-12-17 15:13:23 -0500

    comments here and there

--- react-cards-pokemon/src/PokeDex.js
+++ react-cards-pokemon/src/PokeDex.js
@@ -1,3 +1,5 @@
+// PokeDex.js
+
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 import axios from "axios";
@@ -9,38 +11,53 @@ import "./PokeDex.css";
  * Can also add a new card at random,
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-  // PAM:
-  const [pokemon, setPokemon] = useState([]);
-  // PAM:
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuid() }]);
-  };
-  // PAM:
-  return (
-    <div className="PokeDex">
-      <div className="PokeDex-buttons">
-        <h3>Please select your pokemon:</h3>
-        <PokemonSelect add={addPokemon} />
-      </div>
-      <div className="PokeDex-card-area">
-        {pokemon.map(cardData => (
-          <PokemonCard
-            key={cardData.id}
-            front={cardData.sprites.front_default}
-            back={cardData.sprites.back_default}
-            name={cardData.name}
-            stats={cardData.stats.map(stat => ({
-              value: stat.base_stat,
-              name: stat.stat.name
-            }))}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	/* 
+    Part4
+    - PokeDex also make AJAX requests, but this one’s a bit trickier.
+    - PlayingCardList makes a request to the same endpoint every time, but the endpoint in PokeDex depends on the name of the pokemon.
+    - Figure out a way to modify your useAxios hook so that when you call useAxios you can just provide a base url, and when you want to add to your array of response data in state, you can provide the rest of the url.
+    - Once you’ve done this, refactor PokeDex to use useAxios.
+    - Make sure PlayingCardList still works too! 
+   */
+
+	// PAM: list of pokemon objects that have been caught
+	const [pokemonCaught, setPokemonCaught] = useState([]);
+	// PAM: function to catch a pokemon
+	const addPokemon = async (name) => {
+		const response = await axios.get(
+			`https://pokeapi.co/api/v2/pokemon/${name}/`
+		);
+		// PAM: update the state for the list of pokemon caught
+		setPokemonCaught((pokemonCaught) => [
+			...pokemonCaught,
+			{ ...response.data, id: uuid() },
+		]);
+	};
+
+	// PAM: render
+	return (
+		<div className="PokeDex">
+			<div className="PokeDex-buttons">
+				<h3>Please select your pokemon:</h3>
+				{/* Pokemon select button with function to add pokemon object to our pokemon list state as prop */}
+				<PokemonSelect add={addPokemon} />
+			</div>
+			<div className="PokeDex-card-area">
+				{pokemonCaught.map((cardData) => (
+					<PokemonCard
+						key={cardData.id}
+						front={cardData.sprites.front_default}
+						back={cardData.sprites.back_default}
+						name={cardData.name}
+						stats={cardData.stats.map((stat) => ({
+							value: stat.base_stat,
+							name: stat.stat.name,
+						}))}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default PokeDex;
--- react-cards-pokemon/src/PokemonSelect.js
+++ react-cards-pokemon/src/PokemonSelect.js
@@ -1,27 +1,36 @@
+// PokemonSelect.js 
+
 import React, { useState } from "react";
 import pokemonList from "./pokemonList";
 import { choice } from "./helpers";
 
 /* Select element to choose from common pokemon. */
 function PokemonSelect({ add, pokemon = pokemonList }) {
-  const [pokeIdx, setPokeIdx] = useState(0);
-  const handleChange = evt => {
-    setPokeIdx(evt.target.value);
-  };
+  
+	// PAM: Pokemon id for the pokemon we want to add when we click add buttons
+	const [pokeIdx, setPokeIdx] = useState(0);
 
-  return (
-    <div>
-      <select onChange={handleChange}>
-        {pokemon.map((p, idx) => (
-          <option key={idx} value={idx}>
-            {p}
-          </option>
-        ))}
-      </select>
-      <button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button>
-      <button onClick={() => add(choice(pokemon))}>I'm feeling lucky</button>
-    </div>
-  );
+	const handleChange = (evt) => {
+		// console.log("CHANGE EVENT: ", evt);
+    // PAM: change the pokemon indx state when we change what pokemon we want to catch to that index on our pokemon list array.
+		setPokeIdx(evt.target.value);
+	};
+
+	return (
+		<div>
+			{/* selector for all the pokemon ids, when sleector changes the value of the pokemon name shown and index chosen changes aswell  */}
+			<select onChange={handleChange}>
+				{pokemon.map((p_name, idx) => (
+					<option key={idx} value={idx}>
+						{p_name}
+					</option>
+				))}
+			</select>
+			{/* add pokemon buttons, where on click it will look for the index of the pokemon on our imported pokemon array and pass its name into the add function */}
+			<button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button>
+			<button onClick={() => add(choice(pokemon))}>I'm feeling lucky</button>
+		</div>
+	);
 }
 
 export default PokemonSelect;
